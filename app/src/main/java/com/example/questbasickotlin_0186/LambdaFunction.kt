4b71f8f9725836dc73eb740a455a3e7d6b1569ff
cahyadie <cahyadiilham12@gmail.com>
2024-10-12 23:09:41 +0700

    fun uppercaseString

---
+++ app/src/main/java/com/example/questbasickotlin_0186/LambdaFunction.kt
@@ -0,0 +1,10 @@
+package com.example.questbasickotlin_0186
+
+//Lambda Function
+// Lambda function adalah fungsi yang tidak memiliki nama
+// lambda fuction biasanya digunakan untuk membuat fungsi yang sederhana
+//lamda function menggunakan tanda panah (->) untuk memisakan parameter dan body fungsi
+
+fun uppercaseString(string: String): String {
+    return string.uppercase()
+}
