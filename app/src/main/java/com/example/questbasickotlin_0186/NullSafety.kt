4bafe1e0642aadff4513a8c94c9a2dbb132d4055
cahyadie <cahyadiilham12@gmail.com>
2024-10-12 23:17:52 +0700

    fun nullSafety

---
+++ app/src/main/java/com/example/questbasickotlin_0186/NullSafety.kt
@@ -0,0 +1,25 @@
+package com.example.questbasickotlin_0186
+
+fun nullSafety(){
+    // neverNull has String type
+    var neverNull: String = "This can't be null"
+
+    //Throws a complier error
+    // neverNull = null
+
+    // nullable has nullable String type
+    var nullable: String? = "you can keep a null her e"
+    //this is OK
+    nullable = null
+
+    //check for null in conditions
+    if (neverNull === null){
+        println("inferredNonNull is null")
+    } else {
+        println("inferredNonNull is not null")
+    }
+
+    // safe call operator
+    println(neverNull.length) // 18
+    println(nullable?.length) // null
+}
