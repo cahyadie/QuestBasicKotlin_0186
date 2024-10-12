7014ce9b18083fb734291ee4ddfd683ff4e745c1
cahyadie <cahyadiilham12@gmail.com>
2024-10-12 22:50:29 +0700

    DataClass Done

---
+++ app/src/main/java/com/example/questbasickotlin_0186/DataClass.kt
@@ -0,0 +1,23 @@
+package com.example.questbasickotlin_0186
+
+data class DataClass(
+    val id: Int,
+    val email: String
+)
+
+fun main(){
+    val data = DataClass(1, "indra@gmail.com")
+    println(data)
+
+    //Fungsi Equals
+    val data2 = DataClass(1, "makan@gmail.com")
+    println(data == data2) //false
+
+    //fungsi copy
+    val data3 = data.copy()
+    println(data3)
+
+    // fungsi copy dengan perubahan
+    val data4 = data.copy(email = "sahar@gmail.com")
+    println(data4)
+}
