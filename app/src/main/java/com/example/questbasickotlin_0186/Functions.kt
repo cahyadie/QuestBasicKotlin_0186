5f75ffcfe05226f14b36bb116e104eda96eb8409
cahyadie <cahyadiilham12@gmail.com>
2024-10-12 23:04:10 +0700

    Funnction donn

--- app/src/main/java/com/example/questbasickotlin_0186/Functions.kt
+++ app/src/main/java/com/example/questbasickotlin_0186/Functions.kt
@@ -18,3 +18,22 @@ fun withNamedArgument(name: String, age: Int) {
     println("== withNamedArgument ==")
     println("Hello, $name You are $age years old.")
 }
+
+//default parameter value
+//default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name:String = "Indra", age: Int){
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name You are $age years old.")
+}
+
+fun witReturn(panjang : Int, lebar : Int) {
+    return println(panjang*lebar)
+}
+
+fun main(){
+    withoutParameter()
+    withParameter("Jhon")
+    withNamedArgument(name = "John. age = 30")
+    withDefaultParameter(age = 25)
+}
