dacbdb1f0347f47dfff98d6e0a483ce2996f5af3
cahyadie <cahyadiilham12@gmail.com>
2024-10-12 21:24:07 +0700

    class done

--- app/src/main/java/com/example/questbasickotlin_0186/Class.kt
+++ app/src/main/java/com/example/questbasickotlin_0186/Class.kt
@@ -7,5 +7,15 @@ class Contact(val id: Int, email: String)
 fun main(){
     val contact = Contact(1, "mary@gmail.com")
 
+    // Prints the value of the property: email
+    println(contact.email)
+    
+    // Updates the value of the propert: email
+    contact.email = "jane@gmail.com"
+
+    // Prints the new value of the property: email
+    println(contact.email)
+
+
 }
 
