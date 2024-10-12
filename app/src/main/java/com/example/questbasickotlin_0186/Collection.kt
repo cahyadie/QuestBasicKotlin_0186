5aad1fb7f65c2213cd476890a3fad1f4a50e92ee
cahyadie <cahyadiilham12@gmail.com>
2024-10-12 22:29:56 +0700

    collection 3

--- app/src/main/java/com/example/questbasickotlin_0186/Collection.kt
+++ app/src/main/java/com/example/questbasickotlin_0186/Collection.kt
@@ -51,3 +51,33 @@ fun ContohSet() {
     val shapeLocked: Set<String> = shape
     println(shapeLocked)
 }
+
+fun ContohMap() {
+    println()
+    println("=== Map ===")
+    // Map Read-Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(readOnlyShape)
+
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Map Read-Only
+    val shapeLocked: Map<String, Int> = shape
+    println(shapeLocked)
+}
+
+fun main() {
+    contohlist()
+    ContohSet()
+    ContohMap()
+}
